933fd6fa06199b1a7b540bd49fbbec427c3cf646
MKolev03 <mihail.kolev@stamsoft.com>
2025-04-21 16:56:22 +0300

    Added more info to details pokemon

--- PokeBrowse/PokeBrowse/Data/PokemonDetails.swift
+++ PokeBrowse/PokeBrowse/Data/PokemonDetails.swift
@@ -12,31 +12,71 @@ struct PokemonDetails: Decodable {
     let name: String
     let height: Int
     let weight: Int
+    let baseExperience: Int
     let types: [String]
+    let abilities: [String]
+    let stats: [PokemonStat]
 
     var imageURL: String {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png"
     }
 
     private enum CodingKeys: String, CodingKey {
-        case id, name, height, weight, typesContainer = "types"
+        case id, name, height, weight, baseExperience = "base_experience"
+        case typesContainer = "types"
+        case abilitiesContainer = "abilities"
+        case statsContainer = "stats"
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+
         id = try container.decode(Int.self, forKey: .id)
         name = try container.decode(String.self, forKey: .name)
         height = try container.decode(Int.self, forKey: .height)
         weight = try container.decode(Int.self, forKey: .weight)
+        baseExperience = try container.decode(Int.self, forKey: .baseExperience)
 
         let typesArray = try container.decode([TypeSlot].self, forKey: .typesContainer)
         types = typesArray.map { $0.type.name }
+
+        let abilityArray = try container.decode([AbilitySlot].self, forKey: .abilitiesContainer)
+        abilities = abilityArray.map { $0.ability.name }
+
+        let statArray = try container.decode([StatSlot].self, forKey: .statsContainer)
+        stats = statArray.map { PokemonStat(name: $0.stat.name, baseValue: $0.baseStat) }
     }
 }
 
 struct TypeSlot: Codable {
     let slot: Int
     let type: NamedAPIResource
+}
+
+struct AbilitySlot: Codable {
+    let ability: NamedAPIResource
+    let isHidden: Bool?
+
+    private enum CodingKeys: String, CodingKey {
+        case ability
+        case isHidden = "is_hidden"
+    }
+}
+
+struct StatSlot: Codable {
+    let baseStat: Int
+    let stat: NamedAPIResource
+
+    private enum CodingKeys: String, CodingKey {
+        case baseStat = "base_stat"
+        case stat
+    }
+}
+
+struct PokemonStat: Identifiable {
+    let id = UUID()
+    let name: String
+    let baseValue: Int
 }
 
 struct NamedAPIResource: Codable {
--- PokeBrowse/PokeBrowse/UserInterface/PokemonList/PokemonDetailView.swift
+++ PokeBrowse/PokeBrowse/UserInterface/PokemonList/PokemonDetailView.swift
@@ -14,44 +14,75 @@ struct PokemonDetailView: View {
         ZStack {
             Color(.systemGroupedBackground).ignoresSafeArea()
 
-            VStack {
-                title
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    title
 
-                if viewModel.isLoading {
-                    ProgressView()
-                } else if let details = viewModel.details {
-                    AsyncImage(url: URL(string: details.imageURL)) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 150, height: 150)
-                    } placeholder: {
+                    if viewModel.isLoading {
                         ProgressView()
-                    }
-
-                    pageTitle(details.name.capitalized)
-
-                    TypographyText(text: "ID: \(details.id)", typography: .bodyRegular)
-                    TypographyText(text: "Height: \(details.height)", typography: .bodyRegular)
-                    TypographyText(text: "Weight: \(details.weight)", typography: .bodyRegular)
-
-                    HStack {
-                        TypographyText(text: "Types:", typography: .bodyMedium)
-
-                        ForEach(details.types, id: \.self) { type in
-                            TypographyText(text: "Types:", typography: .bodyRegular)
-                                .padding(6)
-                                .background(Color.blue.opacity(0.2))
-                                .cornerRadius(8)
+                    } else if let details = viewModel.details {
+                        
+                        AsyncImage(url: URL(string: details.imageURL)) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 150, height: 150)
+                                .padding(.top)
+                        } placeholder: {
+                            ProgressView()
                         }
-                    }
 
-                    Spacer()
-                } else {
-                    EmptyView()
+                        pageTitle(details.name.capitalized)
+
+                        Group {
+                            TypographyText(text: "ID: \(details.id)", typography: .bodyRegular)
+                            TypographyText(text: "Height: \(details.height)", typography: .bodyRegular)
+                            TypographyText(text: "Weight: \(details.weight)", typography: .bodyRegular)
+                            TypographyText(text: "Base Experience: \(details.baseExperience)", typography: .bodyRegular)
+                        }
+
+                        VStack(alignment: .leading, spacing: 4) {
+                            TypographyText(text: "Types:", typography: .bodyMedium)
+                            HStack {
+                                ForEach(details.types, id: \.self) { type in
+                                    TypographyText(text: type.capitalized, typography: .bodyRegular)
+                                        .padding(6)
+                                        .background(Color.blue.opacity(0.2))
+                                        .cornerRadius(8)
+                                }
+                            }
+                        }
+
+                        Divider()
+
+                        VStack(alignment: .leading, spacing: 4) {
+                            TypographyText(text: "Abilities:", typography: .bodyMedium)
+                            ForEach(details.abilities, id: \.self) { ability in
+                                TypographyText(text: ability.capitalized, typography: .bodyRegular)
+                            }
+                        }
+
+                        Divider()
+
+                        VStack(alignment: .leading, spacing: 4) {
+                            TypographyText(text: "Stats:", typography: .bodyMedium)
+                            ForEach(details.stats) { stat in
+                                HStack {
+                                    TypographyText(text: stat.name.capitalized, typography: .bodyRegular)
+                                    Spacer()
+                                    TypographyText(text: "\(stat.baseValue)", typography: .bodyRegular)
+                                }
+                            }
+                        }
+
+                        Spacer()
+                    } else {
+                        Text("Unable to load Pokémon data.")
+                            .foregroundColor(.secondary)
+                    }
                 }
+                .padding()
             }
-            .padding()
         }
         .onAppear {
             viewModel.fetchDetails()
@@ -79,6 +110,8 @@ struct PokemonDetailView: View {
                         .foregroundColor(.red)
                 }
             }
+            .padding(.horizontal)
+            .padding(.top, 12)
         }
     }
 }
